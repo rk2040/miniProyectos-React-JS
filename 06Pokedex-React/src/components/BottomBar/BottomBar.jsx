c0dbf1508edd488d7ad3bef8f5aea2687d4cc272
rk2040 <rpardo2040@gmail.com>
2023-06-20 18:15:41 -0300

    consumiendo API y completando 000

--- 06Pokedex-React/src/components/BottomBar/BottomBar.jsx
+++ 06Pokedex-React/src/components/BottomBar/BottomBar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./BottomBar.css";
 
 const BottomBar = () => {
     return (
-        <div>BottomBar</div>
-    )
-}
+        <>
+        <div className="bottom">
+            <div className="bottom-btn">
+                <p className="bg-circle-white">A</p>
+                <p>See Details</p>
+            </div>
+            <div className="bottom-btn">
+                <p className="bg-circle-white">X</p>
+                <p>Habitat</p>
+            </div>
 
-export default BottomBar
+            <div className="bottom-btn">
+                <p className="bg-circle-white">Y</p>
+                <p>Sort</p>
+            </div>
+
+            <div className="bottom-btn">
+                <p className="bg-circle-white">+</p>
+                <p>See Evaluation</p>
+            </div>
+
+            <div className="bottom-btn">
+                <p className="bg-circle-white">B</p>
+                <p>Back</p>
+            </div>
+
+        </div>
+        </>
+    );
+};
+
+export default BottomBar;
--- 06Pokedex-React/src/components/TopBar/TopBar.jsx
+++ 06Pokedex-React/src/components/TopBar/TopBar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './TopBar.css'
+import pokeball from '../../img/pokeball.png'
+import pokeballcolor from '../../img/pokeballcolor.png'
 
 const TopBar = () => {
     return (
-        <div>TopBar</div>
+        <>
+        <div className='title'>
+            <div className="title__left">
+                <p>Pokedex</p>
+                <div className='caught-seen'>
+                    <div className='caught'>
+                        <img src={pokeballcolor} alt="Pokeball" style={{width:'30px', marginRight:'10px'}} />
+                        <p>438</p>
+                    </div>
+                    <div className='seen'>
+                        <img src={pokeball} alt="Pokeball" style={{width:'30px', marginRight:'10px'}} />
+                        <p>649</p>
+
+                    </div>
+                </div>
+
+            </div>
+            <p style={{color:'white'}}>A ➔ Z</p>
+        </div>
+        </>
     )
 }
 
