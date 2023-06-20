ae52d1571d24dd868e75c8cb91aa47fe207aff6d
rk2040 <rpardo2040@gmail.com>
2023-06-20 20:42:42 -0300

    Math Random y paso de emojis random

--- 07MemoblockJuego-React/src/App.jsx
+++ 07MemoblockJuego-React/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Tablero from './components/Tablero/Tablero'
+const emojiList = [... '💀👻🧛🌮🎱🍬🍕🦖']
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [memoBloquesBarajeados, setMemoBloquesBarajeados] = useState([])
+
+  useEffect( ()=>{
+    const barajadoEmojiList = barajarArray([... emojiList, ... emojiList])
+    setMemoBloquesBarajeados(barajadoEmojiList.map( (emoji, i)=>(
+      {index: i, emoji, flipped: false}
+    )))
+  }, [])
+
+  const barajarArray = (a)=>{
+    for(let i=a.length-1; i>0; i--){
+      const j = Math.floor(Math.random()*(i+1));
+      [a[i], a[j]] = [a[j], a[i]];
+      console.log(a)
+    }
+    return a;
+  }
 
   return (
     <>
-      <Tablero/>
+      <Tablero memoBlocks={memoBloquesBarajeados} />
     </>
   )
 }
