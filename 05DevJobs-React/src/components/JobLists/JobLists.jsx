1b3f75d61e4218ce7a673a394c86d0843361fbc5
rk2040 <rpardo2040@gmail.com>
2023-06-19 22:47:51 -0300

    Buscador

--- 05DevJobs-React/src/components/JobLists/JobLists.jsx
+++ 05DevJobs-React/src/components/JobLists/JobLists.jsx
@@ -4,25 +4,60 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
 const JobLists = () => {
-    const [jobData, setJobData] = useState(jobs);
+    const [jobData, setJobData] = useState(jobs);   //BaseDatos (tipo json)
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchByLocation, setSearchByLocation] = useState('');
+
+    const searchTermValue = searchTerm.toLowerCase();
+
+    const locationSearchHandler = ()=>{
+        const filterData = jobs.filter( (job)=>(
+            job.location.toLowerCase().includes(searchByLocation.toLowerCase()))
+        )
+        setJobData(filterData)
+    }
 
     return (
         <>
-        <section>
-            <div className='job__item'>
-                {jobData.map( (item)=>(
-                    <div key={item.id} >
-                        <div className='job__content'>
-                            <h6> {item.postedAt} - {item.contract} </h6>
-                            <h1> 
-                                <Link to={`/jobs/${item.position}`}>
-                                    {item.position}
-                                </Link>
-                            </h1>
-                            <p> {item.company} </p>
+        <section className='job__list'>
+            <div className='container'>
+                <div className='job__list__wrapper'>
+                    <div className='search__panel'>
+                        <div className='search__panel-01'>
+                            <input type="text" placeholder='Busqueda por Puesto, empresa' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+                        </div>
+                        <div className='search__panel-02'>
+                            <input type="text" placeholder='Busqueda por Ubicación' value={searchByLocation} onChange={(e)=>setSearchByLocation(e.target.value)}/>
+                            <button onClick={locationSearchHandler}>Buscar</button>
                         </div>
                     </div>
-                ))}
+                    <div className='job__wrapper'>
+                        {jobData ?.filter( (job)=>{
+                            if(searchTerm === '') return job;
+                            if(job.position.toLocaleLowerCase().includes(searchTermValue) || job.company.toLocaleLowerCase().includes(searchTermValue))
+                            return job;
+                        })
+                        .map( (item)=>(
+                            <div className='job__item' key={item.id} >
+                                <div className='job__content'>
+                                    <h6> {item.postedAt} - {item.contract} </h6>
+                                    <h1> 
+                                        <Link to={`/jobs/${item.position}`}>
+                                            {item.position}
+                                        </Link>
+                                    </h1>
+                                    <p> {item.company} </p>
+                                    <div className='location'>
+                                        <p>
+                                            Location: <span>{item.location}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                        }
+                    </div>
+                </div>
             </div>
         </section>
         </>
