a2806f5b25d5f1a1c1935f87f18757cfcc7aa94d
rk2040 <rpardo2040@gmail.com>
2023-06-21 20:08:11 -0300

    paso props, uso stopPropagation en Modal y estilos

--- 08ModalPage-React/src/App.jsx
+++ 08ModalPage-React/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import Modal from './components/Modal/Modal'
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <>
     <div>
-      <button>Modal</button>
-      <Modal/>
+      <button onClick={()=> setOpenModal(true)} >Modal</button>
+      <Modal open={openModal} onClose={()=> setOpenModal(false)} />
     </div>
     </>
   )
--- 08ModalPage-React/src/components/Modal/Modal.jsx
+++ 08ModalPage-React/src/components/Modal/Modal.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './Modal.scss'
+import logo from '../../img/paypal_logo.png'
 
-const Modal = () => {
+const Modal = ({open, onClose}) => {
+    if(!open) return;
+
     return (
-        <div>Modal</div>
+        <>
+        <section className='contenedor' onClick={onClose} >
+            <div className='sub-contenedor' onClick={(e)=>{e.stopPropagation()}} >  {/* //stopPropagation hace que el evento (onClick en este caso) no tenga efecto en este contenedor */}
+                <div className='contenido'>
+                    <div className='icon'>
+                        <img src={logo} alt="PayPal" width='48px'/>
+                    </div>
+                    <div className='mensaje'>
+                        <div className='title'>
+                            <h3>
+                                Pago realizado con exito
+                            </h3>
+                            <div className='text'>
+                                Gracias por su compra
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='actions'>
+                    <button className='btn-descargar'>Descargar</button>
+                    <button className='btn-volver' onClick={onClose} >Volver</button>
+                </div>
+            </div> 
+
+        </section>
+        </>
     )
 }
 
