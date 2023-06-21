f6f573260d20dd8b0ae4b933f8d856f5fec9bb65
rk2040 <rpardo2040@gmail.com>
2023-06-21 19:39:15 -0300

    1er commit

---
+++ 08ModalPage-React/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Modal from './components/Modal/Modal'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <button>Modal</button>
+      <Modal/>
+    </div>
+    </>
+  )
+}
+
+export default App
