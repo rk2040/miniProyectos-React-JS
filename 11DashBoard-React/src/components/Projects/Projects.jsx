8ae224abf7f9e1d627f884fdb2c64a9cf94efa51
rk2040 <rpardo2040@gmail.com>
2023-06-23 21:57:08 -0300

    Empezando componente Projects

---
+++ 11DashBoard-React/src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import { cardShadow, hoverEffect, themeColor } from '../../utils/utils'
+
+const Projects = () => {
+    return (
+        <div>Projects</div>
+    )
+}
+
+export default Projects
--- 11DashBoard-React/src/pages/Home.jsx
+++ 11DashBoard-React/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Ganancias from "../components/Ganancias/Ganancias";
+import Info from "../components/Info/Info";
 
 const Home = () => {
     return (
@@ -10,8 +11,12 @@ const Home = () => {
                     <SectionA>
                         <ColumnA1>
                             <Ganancias></Ganancias>
+                            <Info></Info>
                         </ColumnA1>
-                        <ColumnA2></ColumnA2>
+
+                        <ColumnA2>
+                            <TitleText>Tus Proyectos</TitleText>
+                        </ColumnA2>
                     </SectionA>
                 </SubContainer>
             </Container>
@@ -24,7 +29,8 @@ export default Home;
 const Container = styled.div`
     width: 80%;
     border-bottom-right-radius: 2rem;
-    background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
+    /* background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%); */
+    background: linear-gradient(to bottom right, #006100 0%, #006100 70%);
     border-top-right-radius: 2rem;
     margin: 1rem 8rem 1rem 4rem;
     @media screen and (min-width: 320px) and (max-width: 1080px) {
@@ -70,6 +76,7 @@ const ColumnA1 = styled.div`
         width: 100%;
     }
 `;
+
 const ColumnA2 = styled.div`
     display: flex;
     flex-direction: column;
@@ -79,5 +86,47 @@ const ColumnA2 = styled.div`
         height: max-content;
         justify-content: center;
         align-items: center;
+    }
+`;
+const TitleText = styled.h3`
+    height: 20%;
+`;
+
+const SectionB = styled.div`
+    display: flex;
+    gap: 2rem;
+    height: 26vh;
+    @media screen and (min-width: 320px) and (max-width: 1080px) {
+        flex-direction: column;
+        height: max-content;
+        width: 100%;
+    }
+`;
+const ColumnB1 = styled.div`
+    @media screen and (min-width: 320px) and (max-width: 1080px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+    }
+`;
+const ColumnB2 = styled.div`
+    @media screen and (min-width: 320px) and (max-width: 1080px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+`;
+const InvoiceContainer = styled.div`
+    height: 60%;
+    @media screen and (min-width: 320px) and (max-width: 1080px) {
+        height: max-content;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
     }
 `;
