b515020f397dd95edd590ce7b4aa67d1c3bce3e9
rk2040 <rpardo2040@gmail.com>
2023-06-24 21:01:16 -0300

    Definiendo variables globales e inst fontsource/sora

--- 12NftPage-React/src/pages/MenuPrincipal.jsx
+++ 12NftPage-React/src/pages/MenuPrincipal.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navigation from '../components/Navigation/Navigation'
 
 const MenuPrincipal = () => {
     return (
-        <div>MenuPrincipal</div>
+        <>
+        <main>
+            <Navigation/>
+        </main>
+        </>
     )
 }
 
---
+++ 12NftPage-React/src/styles/GlobalStyles.jsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+import "@fontsource/sora"
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        font-family: 'Sora', sans-serif;
+    }
+    h1,h2,h3,h4,h5,h6{
+        margin: 0;
+        padding: 0;
+    }
+    a{
+        color: inherit;
+        text-decoration: none;
+    }
+
+`
