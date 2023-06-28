15595dc5a1fb95e1a19f333a9b22373c60f85913
rk2040 <rpardo2040@gmail.com>
2023-06-28 19:57:35 -0300

    component Button y sus estilos

--- 12NftPage-React/src/components/TypeWriterText/TypeWriterText.jsx
+++ 12NftPage-React/src/components/TypeWriterText/TypeWriterText.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import Typewriter from 'typewriter-effect'
+import Button from '../Button/Button'
 
 const TypeWriterText = () => {
     return (
+        <>
         <Title>
-            Aprende react creando proyectos
+            Aprende react creando proyectos 
             <Typewriter
                 options={
                     {
@@ -31,6 +33,14 @@ const TypeWriterText = () => {
             }
             />
         </Title>
+
+        <SubTitle>
+            React es cool 💙
+        </SubTitle>
+        <ButtonContainer>
+            <Button text='Explorar cursos' link='http://www.youtube.com' ></Button>
+        </ButtonContainer>
+        </>
     )
 }
 
@@ -64,5 +74,37 @@ const Title = styled.h2`
     }
     @media (max-width: 40em){
         width:90%;
+    }
+`
+
+const SubTitle = styled.h3`
+    font-size: ${(props)=> props.theme.fontlg};
+    text-transform: capitalize;
+    color: ${(props)=> `rgba(${props.theme.textRgba}, 0.6)`};
+    font-weight: 600;
+    margin-bottom: 1rem;
+    width: 80%;
+    align-self: flex-start;
+
+    @media (max-width: 40em){
+        font-size: ${(props)=> props.theme.fontmd};
+    }
+    @media (max-width: 48em){
+        align-self: center;
+        text-align: center;
+    }
+`
+
+const ButtonContainer = styled.div`
+    width: 80%;
+    align-self: flex-start;
+
+    @media (max-width: 48em){
+        align-self: center;
+        text-align: center;
+        button{
+            margin: 0 auto;
+
+        }
     }
 `
